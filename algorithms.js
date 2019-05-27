d320a6d041b1783869eb5f56056055a6ef6d7fc7
Terry Threatt <terry.threatt@gmail.com>
2019-05-27 11:12:58 -0500

    solved algorithm 5

--- algorithms.js
+++ algorithms.js
@@ -46,3 +46,21 @@ function findLongestWordLength(str) {
 
 const algofour = findLongestWordLength("The quick brown fox jumped over the lazy dog");
 console.log("algorithm 4 = " + algofour);
+
+// 5. Return largest numbers in array
+function largestOfFour(arr) {
+  var results = []; 
+  for (var n = 0; n < arr.length; n++) {
+    var largestNumber = arr[n][0];
+    for (var sb = 1; sb < arr[n].length; sb++) {
+      if(arr[n][sb] > largestNumber) { 
+        largestNumber = arr[n][sb];
+      }
+    }
+    results[n] = largestNumber;
+  }
+  return results;
+}
+
+const algoFive = largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+console.log("algorithm 5 = " + algoFive);
